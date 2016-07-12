c536f079cea52980b3bb850f5611aa8fa3be335a
shaowu <shwgui@niub.la>
2016-07-12 12:20:08 +0800

    add comment

--- replaceForDir.sh
+++ replaceForDir.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+#  批量替换文本
 dir=`dirname $0`
 dir=`cd $dir;pwd`
 
-DIR=$1
-src=$2
-des=$3
+if [ $# -lt 3 ] ; then
+    echo "usage dir, src , des"
+    exit 1
+fi
+DIR=$1  # 需要替换的目录
+src=$2  # 原始文本
+des=$3  # 目标文本
 
 echo "dir:$DIR,src:$src,des:$des"
 sed -i "s/$src/$des/g" `grep -e "$des" -rl $DIR`
